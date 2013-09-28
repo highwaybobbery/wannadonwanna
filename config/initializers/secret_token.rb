4a7d5ddabf37d97cdb5af51f146653c8ce4d43f2
Alex Berry <highwaybobbery@gmail.com>
2013-09-27 23:43:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wannadonwanna::Application.config.secret_key_base = '078003d227b1c0a9a3e069d3158a0234cda0ff132cf97bbb34c158fcbbc9a168675d8d5c5fc23b970df512e598e2c65fd02a78eb161edf733e6c2d4c431880f7'
