4408971c7423563ea45225b0a8df866d31853c56
Alex Berry <highwaybobbery@gmail.com>
2013-09-28 15:13:24 -0400

    added welcome form specs

--- test/integration/visitor_test.rb
+++ test/integration/visitor_test.rb
@@ -3,13 +3,48 @@ class VisitorExperience < ActionDispatch::IntegrationTest
   before { visit root_path }
   describe "Visitor to homepage" do
 
-    it 'must have name prompt' do
-      assert page.has_content?('Hi, my name is:')
+    describe 'name' do
+      it 'has name prompt' do
+        assert page.has_content?('Hi, my name is:')
+      end
+
+      it 'has name input' do
+        assert page.has_css?('input.name')
+      end
     end
 
-    it "must have name input" do
-      assert page.has_css?('input.name')
-    end
+    describe 'desirability indicator' do
+      it 'has desirability prompt' do
+        assert page.has_content?(', and I')
+      end
 
+      describe 'desirability level selector' do
+        it 'is present on the page' do
+          assert page.has_css?('select.severity')
+        end
+
+        ['kinda', 'really', 'really really'].each do |option|
+          it "has  #{option} option" do
+            within(page.find 'select.severity' )do
+              assert page.has_content?(option)
+            end
+          end
+        end
+      end
+
+      describe 'attitude selector' do
+        it 'is present on the page' do
+          assert page.has_css?('select.attitude')
+        end
+
+        ['wanna', 'donwanna'].each do |option|
+          it "has #{option} option" do
+            within(page.find 'select.attitude' )do
+              assert page.has_content?(option)
+            end
+          end
+        end
+      end
+    end
   end
 end
